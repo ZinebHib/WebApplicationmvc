39a3cf84eb678b33c8ca353f3a87f4db6abe598f
ZinebHib <HIBAOUI_ZINEB@emsi-edu.ma>
2024-01-05 12:32:57 +0100

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplicationmvc.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public float Prix { get; set; }
+        public string? Description { get; set; } //on ajoute ? pour dire que desc peut etre nulle ou vide 
+
+      
+    }
+}
